d404f332e13e3dca359e3a4eab0ff16cb6f0ed17
yuan1998 <chizhiyueshu@gmail.com>
2020-03-20 01:35:24 +0800

    修改admin.config,修改侧边栏头部标题显示

--- config/admin.php
+++ config/admin.php
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo'                      => '<b>Laravel</b> admin',
+    'logo'                      => '<b>表里</b>单一',
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ return [
     | '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo-mini'                 => '<b>La</b>',
+    'logo-mini'                 => '<b>表单</b>',
 
     /*
     |--------------------------------------------------------------------------
